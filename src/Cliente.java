635a7e00516989dbb7d62309f3b0624218383052
LucasPanzeraa <lucaspanzera614@gmail.com>
2023-07-14 11:10:29 -0300

    nueva distribucion e implementacion de hashMap

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,65 +1,81 @@
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 public class Cliente {
-    private static final int puertoDelServidor = 60246;
-    private static final int tamañoDelBuffer = 1024;
+    private static final int puertoDelServer = 60246;
+    private static final int TamañoDelBuffer = 1024;
 
-    public static void main(String[] args) {
+    private DatagramSocket clientSocket;
+    private InetAddress serverAddress;
+
+    public Cliente(InetAddress serverAddress) {
         try {
-
-
-            System.out.println("Cliente UDP iniciado");
-
-            while (true) {
-
-                byte[] buffer = new byte[tamañoDelBuffer];
-
-                Scanner scanner = new Scanner(System.in); // se  le pide al usuario que ingrese el nombre del detinatario y mensaje a enviar
-
-                System.out.print("Ingrese el nombre del cliente destino: ");
-                String destino = scanner.nextLine();
-
-                DatagramSocket socketCliente = new DatagramSocket(); // se crea el DatagramSocket para enviar y recibir datos sin establecer una conexion persistente
-                InetAddress direccionServidor = IpDestino(destino);
-
-                System.out.print("Ingrese el mensaje: ");
-                String mensaje = scanner.nextLine();
-
-                String mensajeEnvio = destino + ": " + mensaje;
-                byte[] bufferEnvio = mensajeEnvio.getBytes();  //se crea un buufer para pasar el mensaje a bytes
-
-                DatagramPacket paqueteEnvio = new DatagramPacket(bufferEnvio, bufferEnvio.length, direccionServidor, puertoDelServidor);
-                socketCliente.send(paqueteEnvio);   // se envia el mensaje al servidor con la dirección IP del servidor y el puerto definido.
-
-                DatagramPacket paqueteRecibido = new DatagramPacket(buffer, buffer.length);
-                socketCliente.receive(paqueteRecibido);  // se recibe la respuestra del servidor
-
-                String mensajeRecibido = new String(paqueteRecibido.getData(), 0 , paqueteRecibido.getLength());  // offset es de donde se empiezan a leer los datos
-                System.out.println("Respuesta del servidor: " + mensajeRecibido);
-            }
+            clientSocket = new DatagramSocket();
+            this.serverAddress = serverAddress;
+            System.out.println("Cliente conectado al servidor: " + serverAddress);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static InetAddress IpDestino(String nombreCliente) { // ese metodo sirve para saber a que ip mandar el ACK
+    public void inicio() {
+        try {
+            Thread receiveThread = new Thread(new RecibirMensaje());
+            receiveThread.start();
 
-        if (nombreCliente.equalsIgnoreCase("Lucas")) {     // equalsIgnoreCase es para que se comparen dos strings
-            try {                                                     // sin importar las mayusculas o las minusculas
-                return InetAddress.getByName("172.16.255.220");
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String message = scanner.nextLine();
+                mandarMensaje(message);
             }
-        } else if (nombreCliente.equalsIgnoreCase("Juan")) {
-            try {
-                return InetAddress.getByName("172.16.255.228");
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
+        } finally {
+            if (clientSocket != null) {
+                clientSocket.close();
             }
         }
+    }
 
-        return null; // Si el cliente no se encuentra devuelve null
+    private void mandarMensaje(String message) {
+        try {
+            byte[] sendBuffer = message.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, puertoDelServer);
+            clientSocket.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class RecibirMensaje implements Runnable {
+        @Override
+        public void run() {
+            byte[] receiveBuffer = new byte[TamañoDelBuffer];
+
+            while (true) {
+                try {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                    clientSocket.receive(receivePacket);
+
+                    String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println("Mensaje recibido del servidor: " + receivedMessage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            InetAddress serverAddress = InetAddress.getByName("172.16.255.220");
+
+            Cliente client = new Cliente(serverAddress);
+            client.inicio();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -2,73 +2,102 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Servidor {
-    private static final int puertoDelServidor = 60246;
-    private static final int tamañoDelBuffer = 1024;
+    private static final int puertoServidor = 60246;
+    private static final int tamañoDelBaffer = 1024;
 
-    public static void main(String[] args) {
+    private DatagramSocket serverSocket;
+    private Map<InetAddress, Integer> clients;
+
+
+    public Servidor() {
+
         try {
-            DatagramSocket socketServidor = new DatagramSocket(puertoDelServidor); // escucha en el puerto recibido
-            System.out.println("Servidor UDP iniciado en el puerto " + puertoDelServidor);
-
-            byte[] buffer = new byte[tamañoDelBuffer]; //  buffer para almacenar los datos recibidos.
-
-            while (true) {
-                DatagramPacket paqueteRecibido = new DatagramPacket(buffer, buffer.length); // recibe los mensajes enviados por los clientes
-                socketServidor.receive(paqueteRecibido);
-
-                InetAddress direccionCliente = paqueteRecibido.getAddress(); // se obtiene el puerto y la direccion ip para despues devolver el ACK
-                int puertoCliente = paqueteRecibido.getPort();
-
-                String mensajeRecibido = new String(paqueteRecibido.getData(), 0, paqueteRecibido.getLength());
-                System.out.println("Mensaje recibido de " + direccionCliente + ":" + puertoCliente + ": " + mensajeRecibido);
-
-                String[] partesMensaje = mensajeRecibido.split(":", 2); // dividimos el mensaje en dos por un lado el nombre del cliente de destino
-                if (partesMensaje.length == 2) {                                   // y por el otro el contenido
-                    String destino = partesMensaje[0].trim();
-                    String contenidoMensaje = partesMensaje[1].trim();
-
-
-                    InetAddress direccionDestino = buscarCliente(destino); // se crea un mensaje de ACK concatenando al contenido del mensaje.
-                    if (direccionDestino != null) {
-                        String mensajeAck = "ACK: " + contenidoMensaje;
-                        byte[] bufferAck = mensajeAck.getBytes();   // se combierte el mensaje en un array de bytes para porder mandarlo
-
-                        DatagramPacket paqueteAck = new DatagramPacket(bufferAck, bufferAck.length, direccionDestino, puertoCliente);
-                        socketServidor.send(paqueteAck);        // se envia el mensaje con el puertop y la direccion ip del mensaje original
-
-                    } else {
-                        System.out.println("Cliente destino no encontrado: " + destino);
-                    }
-                } else {
-                    System.out.println("Formato de mensaje incorrecto: " + mensajeRecibido);
-                }
-            }
+            serverSocket = new DatagramSocket(puertoServidor);
+            clients = new HashMap<>();
+            System.out.println("Servidor listo para recibir conexiones...");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-    // el nombreClinte va a ser la variable destino que pasemos en Cliente por lo qeu tenemos que configrurar ip y nombres por default
-    private static InetAddress buscarCliente(String nombreCliente) { // ese metodo sirve para saber a que ip mandar el ACK
-
-        if (nombreCliente.equalsIgnoreCase("Lucas")) {     // equalsIgnoreCase es para que se comparen dos strings
-            try {                                                     // sin importar las mayusculas o las minusculas
-                return InetAddress.getByName("172.16.255.220");
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            }
-        } else if (nombreCliente.equalsIgnoreCase("Juan")) {
+    public void start() {
+        byte[] receiveBuffer = new byte[tamañoDelBaffer];
+        while (true) {
             try {
-                return InetAddress.getByName("172.16.255.228");
-            } catch (UnknownHostException e) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                serverSocket.receive(receivePacket);
+
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+
+                String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Mensaje recibido de " + clientAddress + ":" + clientPort + ": " + message);
+
+                if (!clients.containsKey(clientAddress)) {
+                    clients.put(clientAddress, clientPort);
+                }
+
+                // Verificar si el mensaje es un comando de envío de mensaje privado
+                if (message.startsWith("@")) {
+
+                    String[] parts = message.split(" ", 2);
+                    if (parts.length == 2) {
+
+                        String recipientName = parts[0].substring(1);
+                        String privateMessage = parts[1];
+
+                        InetAddress recipientAddress = null;
+                        int recipientPort;
+
+                        for (Map.Entry<InetAddress, Integer> entry : clients.entrySet()) {
+                            if (entry.getValue().equals(clientPort)) {
+
+                                recipientAddress = entry.getKey();
+                                break;
+                            }
+                        }
+
+                        if (recipientAddress != null) {
+                            recipientPort = clients.get(recipientAddress);
+                            sendMessageToClient("Mensaje privado de " + clientAddress + ":" + clientPort + ": " +
+                            privateMessage, recipientAddress, recipientPort);
+
+                        }
+                    }
+                }
+                // Enviamos un ACK al cliente
+
+                sendAck(clientAddress, clientPort);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-        return null; // Si el cliente no se encuentra devuelve null
     }
+    private void sendAck(InetAddress clientAddress, int clientPort) throws IOException {
+        String ackMessage = "ACK";
+        byte[] ackBuffer = ackMessage.getBytes();
+
+        DatagramPacket ackPacket = new DatagramPacket(ackBuffer, ackBuffer.length, clientAddress, clientPort);
+        serverSocket.send(ackPacket);
+    }
+    private void sendMessageToClient(String message, InetAddress clientAddress, int clientPort) throws IOException {
+        byte[] sendBuffer = message.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress, clientPort);
+        serverSocket.send(sendPacket);
+    }
+
+
+    public static void main(String[] args) {
+
+        Servidor server = new Servidor();
+
+        server.start();
+
+    }
+
 }
